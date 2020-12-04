5104904bd502bdaf4dd16ef2413d6ddcceaffb07
qmya <qandeelgul90@gmail.com>
2020-12-04 10:26:15 -0500

    use strict mode to find the bug quickly in your code

---
+++ script.js
@@ -0,0 +1,9 @@
+//use  strict mood we use to avoid bugs into our code
+"use strict";
+
+let hasDriversLicense = false;
+
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive.");
